ae2615e84e0fe5051f3d8b831399d56abfa60345
Cosimo Negri <cosimogiovanninegri@gmail.com>
2023-10-03 00:47:36 +0200

    simplify theme context

--- src/contexts/theme.tsx
+++ src/contexts/theme.tsx
@@ -1,19 +1,11 @@
 import React from "react";
+import { useMedia, useLocalStorage } from "react-use";
 
 const LIGHT_THEME = "light";
 const DARK_THEME = "dark";
-const DEFAULT_THEME = LIGHT_THEME;
-const THEME_STORAGE_KEY = "data-theme";
+const THEME_KEY = "data-theme";
 
 type Theme = typeof LIGHT_THEME | typeof DARK_THEME;
-
-const isTheme = (value: unknown): value is Theme => {
-    return value === LIGHT_THEME || value === DARK_THEME;
-};
-
-const getOppositeTheme = (theme: Theme) => {
-    return theme === LIGHT_THEME ? DARK_THEME : LIGHT_THEME;
-};
 
 interface ThemeContextProps {
     theme: Theme;
@@ -22,7 +14,7 @@ interface ThemeContextProps {
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 const ThemeContext = React.createContext<ThemeContextProps>({
-    theme: DEFAULT_THEME,
+    theme: LIGHT_THEME,
     setTheme: () => {},
 });
 
@@ -31,68 +23,23 @@ interface ThemeProviderProps {
 }
 
 export const ThemeContextProvider = ({ children }: ThemeProviderProps) => {
-    const initialStoredTheme = React.useMemo(
-        () => localStorage.getItem(THEME_STORAGE_KEY),
-        [],
-    );
-    const initialTheme = React.useMemo(
-        () =>
-            isTheme(initialStoredTheme) ? initialStoredTheme : DEFAULT_THEME,
-        [initialStoredTheme],
-    );
+    const [theme, setTheme] = useLocalStorage<Theme>(THEME_KEY);
+    const isSystemDark = useMedia(`(prefers-color-scheme: ${DARK_THEME})`);
 
-    const [theme, setTheme] = React.useState<Theme>(initialTheme);
-    const [isThemeInStorage, setIsThemeInStorage] = React.useState(
-        isTheme(initialStoredTheme),
+    const finalTheme = React.useMemo(
+        () => (theme ? theme : isSystemDark ? DARK_THEME : LIGHT_THEME),
+        [theme, isSystemDark],
     );
-    const systemPreference = React.useMemo(
-        () =>
-            window.matchMedia(
-                `(prefers-color-scheme: ${getOppositeTheme(DEFAULT_THEME)})`,
-            ),
-        [],
-    );
-
-    const updateTheme = (theme: Theme) => {
-        setTheme(theme);
-        setIsThemeInStorage(true);
-        document.documentElement.setAttribute(THEME_STORAGE_KEY, theme);
-        localStorage.setItem(THEME_STORAGE_KEY, theme);
-    };
-
-    const updateThemeFromSystemPreference = React.useCallback(() => {
-        if (!isThemeInStorage) {
-            const theme = systemPreference.matches
-                ? getOppositeTheme(DEFAULT_THEME)
-                : DEFAULT_THEME;
-            setTheme(theme);
-            document.documentElement.setAttribute(THEME_STORAGE_KEY, theme);
-        }
-    }, [systemPreference, isThemeInStorage]);
 
     React.useLayoutEffect(() => {
-        document.documentElement.setAttribute(THEME_STORAGE_KEY, initialTheme);
-        updateThemeFromSystemPreference();
-    }, [updateThemeFromSystemPreference, initialTheme]);
-
-    React.useEffect(() => {
-        systemPreference.addEventListener(
-            "change",
-            updateThemeFromSystemPreference,
-        );
-        return () => {
-            systemPreference.removeEventListener(
-                "change",
-                updateThemeFromSystemPreference,
-            );
-        };
-    }, [systemPreference, updateThemeFromSystemPreference]);
+        document.documentElement.setAttribute(THEME_KEY, finalTheme);
+    }, [finalTheme]);
 
     return (
         <ThemeContext.Provider
             value={{
-                theme,
-                setTheme: (theme: Theme) => updateTheme(theme),
+                theme: finalTheme,
+                setTheme,
             }}
         >
             {children}
